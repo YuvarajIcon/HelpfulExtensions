91a188d939311c572940719667cecb4a29e2d7bd
Yuvaraj <yuvaraj@ncompass.in>
2022-05-11 18:52:16 +0530

    added string extension for URL detection

--- HelpfulExtensions/Classes/String + Extensions.swift
+++ HelpfulExtensions/Classes/String + Extensions.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+extension String {
+    var isValidUrl: Bool {
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+            if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
+                return match.range.length == self.utf16.count
+            } else {
+                return false
+            }
+        } catch {
+            return false
+        }
+    }
+}
